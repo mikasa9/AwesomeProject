466eb5c88d8d4aed9af1f656b6f09aad077d9729
Babli Dey <bablihdey@gmail.com>
2018-06-13 21:17:47 +0530

    modalview changes in image and text

--- App.js
+++ App.js
@@ -10,18 +10,18 @@ import {
 } from 'react-native';
 
 const Data = [
-  { id: '1', img: 'http://cdn.playbuzz.com/cdn//b8dc6916-550f-4254-87ca-bc83500cd071/599f8f89-3554-4263-b7b1-822957dcc3bc.jpg', text: 'babli' },
+  { id: '1', img: 'http://cdn.playbuzz.com/cdn//b8dc6916-550f-4254-87ca-bc83500cd071/599f8f89-3554-4263-b7b1-822957dcc3bc.jpg', text: 'levi' },
   { id: '2', img: 'http://cdn.playbuzz.com/cdn//b8dc6916-550f-4254-87ca-bc83500cd071/aed54fd6-5ddb-4254-a685-5b3e4679b045.jpg', text: 'shifa' },
   { id: '3', img: 'http://cdn.playbuzz.com/cdn//b8dc6916-550f-4254-87ca-bc83500cd071/de5b8b66-7fb5-4d58-8d9e-ea9ea4f5f9ff.jpg', text: 'rahul' },
-  { id: '4', img: 'http://cdn.playbuzz.com/cdn//b8dc6916-550f-4254-87ca-bc83500cd071/6ca4bf46-4fa2-4ae6-8f85-2cad61a550d0.jpg', text: 'vijay' },
+  { id: '4', img: 'http://cdn.playbuzz.com/cdn//b8dc6916-550f-4254-87ca-bc83500cd071/6ca4bf46-4fa2-4ae6-8f85-2cad61a550d0.jpg', text: 'luffy' },
   { id: '5', img: 'http://cdn.playbuzz.com/cdn//b8dc6916-550f-4254-87ca-bc83500cd071/e1d3f2db-a3ee-40fb-816e-f979f3291f42.jpg', text: 'sana' },
-  { id: '6', img: 'http://cdn.playbuzz.com/cdn//b8dc6916-550f-4254-87ca-bc83500cd071/ed58c7a1-56c7-4815-a5e8-64394cb4e21b.jpg', text: 'sanya' },
+  { id: '6', img: 'http://cdn.playbuzz.com/cdn//b8dc6916-550f-4254-87ca-bc83500cd071/ed58c7a1-56c7-4815-a5e8-64394cb4e21b.jpg', text: 'mitskuni' },
   { id: '7', img: 'http://cdn.playbuzz.com/cdn//b8dc6916-550f-4254-87ca-bc83500cd071/89f5f4cb-5688-4bd9-8991-559b8c73a276.jpg', text: 'mike' },
-  { id: '8', img: 'http://cdn.playbuzz.com/cdn//b8dc6916-550f-4254-87ca-bc83500cd071/94cbe1e5-21fe-49a6-8e9c-7ba74148dd3d.jpg', text: 'kim' },
+  { id: '8', img: 'http://cdn.playbuzz.com/cdn//b8dc6916-550f-4254-87ca-bc83500cd071/94cbe1e5-21fe-49a6-8e9c-7ba74148dd3d.jpg', text: 'Light' },
   { id: '9', img: 'http://cdn.playbuzz.com/cdn//b8dc6916-550f-4254-87ca-bc83500cd071/0b6471a4-aed0-439d-9a61-1b6eea47ca32.jpg', text: 'kit' },
   { id: '10', img: 'http://cdn.playbuzz.com/cdn//b8dc6916-550f-4254-87ca-bc83500cd071/1e4c960e-a2ad-42ad-8081-7c92ab8afd3e.jpg', text: 'b' },
   { id: '11', img: 'http://cdn.playbuzz.com/cdn//b8dc6916-550f-4254-87ca-bc83500cd071/3c344f0e-d299-4e09-8ba1-32b692620261.jpg', text: 'mikasa' },
-  { id: '12', img: 'http://cdn.playbuzz.com/cdn//b8dc6916-550f-4254-87ca-bc83500cd071/9b194294-a42e-4436-868b-29c734954a13.jpg', text: 'L' },
+  { id: '12', img: 'http://cdn.playbuzz.com/cdn//b8dc6916-550f-4254-87ca-bc83500cd071/9b194294-a42e-4436-868b-29c734954a13.jpg', text: 'Near' },
 ]
 
 
@@ -90,18 +90,18 @@ export default class App extends Component {
         <Modal
           visible={this.state.isModalViewVisible}
           transparent={false}
-          style={{ flex: 1, backgroundColor: 'yellow' }}
+
           onRequestClose={this.toggleModalView}
         >
-
+          <Image style={styles.imageDetail} source={{ uri: this.state.currentClickedItem ? this.state.currentClickedItem.img : '' }} />
           <TouchableOpacity
 
             onPress={this.toggleModalView}
           >
-            <Text style={{ padding: 15 }}>{this.state.currentClickedItem?this.state.currentClickedItem.text:'n/a'}</Text>
+            <Text style={{ padding: 15, fontSize: 30, }}>{this.state.currentClickedItem ? this.state.currentClickedItem.text : 'n/a'}</Text>
           </TouchableOpacity>
 
-            <Image style={styles.imageStyle} source={{ uri:this.state.currentClickedItem?this.state.currentClickedItem.img:'' }}/>
+
         </Modal>
       </View>
     );
@@ -115,7 +115,7 @@ const styles = StyleSheet.create({
 
     flex: 1,
 
-    // backgroundColor:'blue',
+    
   },
   row: {
     padding: 5,
@@ -142,4 +142,13 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
   },
+  imageDetail: {
+
+    resizeMode: 'cover',
+    width: 200,
+    height: 200,
+
+
+  },
+
 })
